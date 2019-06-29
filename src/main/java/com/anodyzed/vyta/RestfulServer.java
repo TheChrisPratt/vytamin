1a6a757704df7b0251249eddbc33561d7688b32c
Chris Pratt <chris@anodyzed.com>
2019-06-29 00:00:21 -0700

    Working XML Resources

--- src/main/java/com/anodyzed/vyta/RestfulServer.java
+++ src/main/java/com/anodyzed/vyta/RestfulServer.java
@@ -5,8 +5,10 @@ import com.anodyzed.vyta.resources.CourseResource;
 import com.anodyzed.vyta.resources.StudentResource;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.lifecycle.ResourceProvider;
@@ -15,6 +17,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
 /**
  * RestfulServer
@@ -25,26 +28,35 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class RestfulServer {
   private static final Logger log = LoggerFactory.getLogger(RestfulServer.class);
 
+  private static final List<Class<?>> resources = Arrays.asList(CourseResource.class,StudentResource.class);
+  private static List<ResourceProvider> providers;
+
   private static List<Class<?>> getResources () {
-    List<Class<?>> resources = new ArrayList<>();
-    resources.add(CourseResource.class);
-    resources.add(StudentResource.class);
     return resources;
   } //getResources
 
   private static List<ResourceProvider> getProviders (ApplicationContext ctx) {
-    List<ResourceProvider> providers = new ArrayList<>();
-    SpringResourceFactory provider = new SpringResourceFactory("springBus");
-    provider.setApplicationContext(ctx);
-    providers.add(provider);
-//    providers.add(SingletonResourceProvider(ctx.getBean(CourseResource.class)));
+    if(providers == null) {
+      Component component;
+      SpringResourceFactory provider;
+      providers = new ArrayList<>();
+      for(Class<?> resource : resources) {
+        if((component = resource.getAnnotation(Component.class)) != null) {
+          provider = new SpringResourceFactory(component.value());
+          provider.setApplicationContext(ctx);
+          providers.add(provider);
+        }
+      }
+    }
     return providers;
+//    return new SingletonResourceProvider(ctx.getBean(CourseResource.class));
   } //getProviders
 
   public static void main (String... args) {
     log.info("Starting Server...");
     ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfiguration.class);
     JAXRSServerFactoryBean factoryBean = new JAXRSServerFactoryBean();
+    factoryBean.setBus((SpringBus)ctx.getBean("springBus"));
     factoryBean.setResourceClasses(getResources());
     factoryBean.setResourceProviders(getProviders(ctx));
     factoryBean.setAddress("http://localhost:8888/");
